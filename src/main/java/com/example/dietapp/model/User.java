932508f5c5c595f75a34787ff4a01fe7e5df61b9
panagiotakrm <pankaramani@gmail.com>
2025-04-14 13:53:37 +0300

    dkfgnng

--- src/main/java/com/example/dietapp/model/User.java
+++ src/main/java/com/example/dietapp/model/User.java
@@ -4,12 +4,13 @@ public class User {
     private double weight, height;
     private int age;
     private String gender;
-
+private int num;
     public User(double weight, double height, int age, String gender) {
         this.weight = weight;
         this.height = height;
         this.age = age;
         this.gender = gender;
+       this.num=num;
     }
 
     public double getWeight() { return weight; }
